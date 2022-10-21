870d3bae71652931ba23750f229d0b4f312af454
PoorneshShiva <hogrider9343@gmail.com>
2022-10-21 19:05:58 +0530

    updated gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -17,17 +17,77 @@ const Gallery = () => {
 			)
 		)
 	);
-	const postsItems = [];
-	images.forEach((each, index) => {
-		// eslint-disable-next-line
+	const pictures = {
+		image1: [
+			"/static/media/WhatsApp Image 2022-10-19 at 5.40.20 PM (1).b5f74127.jpeg",
+			"Descp",
+		],
+		image2: [
+			"/static/media/WhatsApp Image 2022-10-19 at 5.40.20 PM (2).ff660cfe.jpeg",
+			"Descp",
+		],
+		image3: [
+			"/static/media/WhatsApp Image 2022-10-19 at 5.40.20 PM.07922eec.jpeg",
+			"Descp",
+		],
 
+		image5: [
+			"/static/media/WhatsApp Image 2022-10-19 at 5.40.21 PM (2).c70561e7.jpeg",
+			"Descp",
+		],
+
+		image10: [
+			"/static/media/WhatsApp Image 2022-10-19 at 5.40.23 PM (2).2e8c6c4f.jpeg",
+			"Descp",
+		],
+		image11: [
+			"/static/media/WhatsApp Image 2022-10-19 at 5.40.23 PM.08e1f293.jpeg",
+			"Descp",
+		],
+
+		image13: [
+			"/static/media/WhatsApp Image 2022-10-19 at 5.40.24 PM.15289fd8.jpeg",
+			"Descp",
+		],
+		image14: [
+			"/static/media/WhatsApp Image 2022-10-19 at 5.40.25 PM.90d3e836.jpeg",
+			"Descp",
+		],
+		image15: [
+			"/static/media/WhatsApp Image 2022-10-19 at 5.40.26 PM (1).e4943588.jpeg",
+			"Descp",
+		],
+		image16: [
+			"/static/media/WhatsApp Image 2022-10-19 at 5.40.26 PM.97445b41.jpeg",
+			"Descp",
+		],
+		image17: [
+			"/static/media/WhatsApp Image 2022-10-19 at 5.40.27 PM (1).3fc6b1f9.jpeg",
+			"Descp",
+		],
+		image18: [
+			"/static/media/WhatsApp Image 2022-10-19 at 5.40.27 PM (2).eac93cbd.jpeg",
+			"Descp",
+		],
+		image19: [
+			"/static/media/WhatsApp Image 2022-10-19 at 5.40.27 PM.68391ac0.jpeg",
+			"Descp",
+		],
+		image20: [
+			"/static/media/WhatsApp Image 2022-10-19 at 5.40.28 PM.112f472f.jpeg",
+			"Descp",
+		],
+	};
+	const postsItems = [];
+	Object.values(pictures).forEach((each, index) => {
+		// eslint-disable-next-line
 		postsItems.push(
 			<div key={index}>
 				<div className="image-container">
 					<div className="image">
-						<img src={each} alt="" />
+						<img src={each[0]} alt="" />
 					</div>
-					<div className="title"> Description</div>
+					<div className="title"> {each[1]}</div>
 				</div>
 			</div>
 		);
