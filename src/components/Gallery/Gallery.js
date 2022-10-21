f9cf4b98a15d200dcb6c8798b1ee6b06f0a6bd16
PoorneshShiva <hogrider9343@gmail.com>
2022-10-21 19:22:41 +0530

    update

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -11,7 +11,7 @@ const Gallery = () => {
 	const images = Array.from(
 		importAll(
 			require.context(
-				"../../../public/images/gallery-images/gallery-images/gallery-images",
+				"../images/gallery-images/gallery-images/gallery-images",
 				false,
 				/\.(png|jpe?g|svg)$/
 			)
@@ -19,62 +19,62 @@ const Gallery = () => {
 	);
 	const pictures = {
 		image1: [
-			"/static/public/images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.20 PM (1).b5f74127.jpeg",
+			"./images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.20 PM (1).b5f74127.jpeg",
 			"Descp",
 		],
 		image2: [
-			"/static/public/images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.20 PM (2).ff660cfe.jpeg",
+			"./images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.20 PM (2).ff660cfe.jpeg",
 			"Descp",
 		],
 		image3: [
-			"/static/public/images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.20 PM.07922eec.jpeg",
+			"./images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.20 PM.07922eec.jpeg",
 			"Descp",
 		],
 
 		image5: [
-			"/static/public/images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.21 PM (2).c70561e7.jpeg",
+			"./images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.21 PM (2).c70561e7.jpeg",
 			"Descp",
 		],
 
 		image10: [
-			"/static/public/images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.23 PM (2).2e8c6c4f.jpeg",
+			"./images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.23 PM (2).2e8c6c4f.jpeg",
 			"Descp",
 		],
 		image11: [
-			"/static/public/images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.23 PM.08e1f293.jpeg",
+			"./images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.23 PM.08e1f293.jpeg",
 			"Descp",
 		],
 
 		image13: [
-			"/static/public/images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.24 PM.15289fd8.jpeg",
+			"./images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.24 PM.15289fd8.jpeg",
 			"Descp",
 		],
 		image14: [
-			"/static/public/images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.25 PM.90d3e836.jpeg",
+			"./images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.25 PM.90d3e836.jpeg",
 			"Descp",
 		],
 		image15: [
-			"/static/public/images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.26 PM (1).e4943588.jpeg",
+			"./images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.26 PM (1).e4943588.jpeg",
 			"Descp",
 		],
 		image16: [
-			"/static/public/images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.26 PM.97445b41.jpeg",
+			"./images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.26 PM.97445b41.jpeg",
 			"Descp",
 		],
 		image17: [
-			"/static/public/images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.27 PM (1).3fc6b1f9.jpeg",
+			"./images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.27 PM (1).3fc6b1f9.jpeg",
 			"Descp",
 		],
 		image18: [
-			"/static/public/images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.27 PM (2).eac93cbd.jpeg",
+			"./images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.27 PM (2).eac93cbd.jpeg",
 			"Descp",
 		],
 		image19: [
-			"/static/public/images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.27 PM.68391ac0.jpeg",
+			"./images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.27 PM.68391ac0.jpeg",
 			"Descp",
 		],
 		image20: [
-			"/static/public/images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.28 PM.112f472f.jpeg",
+			"./images/gallery-images/WhatsApp Image 2022-10-19 at 5.40.28 PM.112f472f.jpeg",
 			"Descp",
 		],
 	};
@@ -107,7 +107,7 @@ const Gallery = () => {
 					<div className="image-container">
 						<div className="image">
 							<img
-								src="https://m.public/images/gallery-images-amazon.com/images/gallery-images/I/71OebiSOsRL._AC_UY218_.jpg"
+								src="ht./images/gallery-images-amazon.com/images/gallery-images/I/71OebiSOsRL._AC_UY218_.jpg"
 								alt=""
 							/>
 						</div>
